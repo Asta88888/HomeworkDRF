9225bd1e3aa4bc25dbe19b07a3cf2274c0bae8b5
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-08-17 04:57:24 +0300

    add user crud

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from users.apps import UsersConfig
 from django.urls import path
-from users.views import UserProfileView, PaymentViewSet
+from users.views import UserProfileView, PaymentViewSet, UserCreateAPIView, UserListAPIView, UserUpdateAPIView, UserDestroyAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
@@ -11,6 +15,12 @@ router.register(r'payments', PaymentViewSet, basename='payment')
 
 urlpatterns = [
     path("profile/", UserProfileView.as_view(), name="profile"),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('list/', UserListAPIView.as_view(), name='users_list'),
+    path('update/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('destroy/', UserDestroyAPIView.as_view(), name='user_destroy'),
 ]
 
 urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,13 +1,43 @@
 from django.views.generic import DetailView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
-
-from users.models import Payment
-from users.serializer import PaymentSerializer
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from users.models import Payment, User
+from users.serializer import PaymentSerializer, UserSerializer
 
 
 class UserProfileView(DetailView):
     pass
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
