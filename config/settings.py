4984944160018773bc7ba82610f5f71f95c98027
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-08-29 10:49:24 +0300

    add celery beat

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django_filters",
     "users",
     "materials",
+    "django_celery_beat",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,11 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    "deactivate-inactive-users-every-night": {
+        "task": "users.tasks.deactivate_inactive_users",
+        "schedule": crontab(hour=0, minute=0),
+    },
+}
--- materials/tasks.py
+++ materials/tasks.py
@@ -1,7 +1,13 @@
+from datetime import timedelta
+
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
+from django.utils import timezone
+
 from materials.models import Subscription, Course
+from users.models import User
+
 
 @shared_task
 def send_update_notification(course_id):
@@ -17,3 +23,14 @@ def send_update_notification(course_id):
                 recipient_list=[sub.user.email],
                 fail_silently=False,
             )
+
+
+@shared_task
+def deactivate_inactive_users():
+    one_month_ago = timezone.now() - timedelta(days=30)
+    users_to_deactivate = User.objects.filter(
+        last_login__lt=one_month_ago,
+        is_active=True
+    )
+    count = users_to_deactivate.update(is_active=False)
+    return f'Заблокировано {count} пользователей.'
